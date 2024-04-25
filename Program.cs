4c7b2b652380c186922c9699d44098d31fe0dc0b
SidMehraa <160326054+SidMehraa@users.noreply.github.com>
2024-04-25 22:58:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,9 +10,36 @@ namespace CowsAndBulls
     {
         static void Main(string[] args)
         {
-            //Cows and Bulls Starter Framework Code
-
-            Console.ReadLine();
+          Random random = new Random(); 
+    int Answer; 
+    do 
+    { 
+        Answer = random.Next(1023, 9876); 
+    } while (answer.ToString().Distinct().Count() < 4 ); 
+    int cows, bulls; 
+    do 
+    { 
+        Console.WriteLine("Guess what four digit number it is"); 
+        string Guess = Console.ReadLine(); 
+        while (Guess.Length != 4 || Guess[0] == '0' || Guess.Distinct().Count() < 4) 
+        { 
+  
+            Console.WriteLine("That guess wasn't valid, please try again with a 4 digit number, with unique numbers everywhere and not starting at 0"); 
+            Guess = Console.ReadLine(); 
+        } 
+        NumOfCows = 0; 
+        NumOfBulls = 0; 
+        for (int i = 0; i < 4; i++) 
+        { 
+            if (Guess[i] == Answer.ToString()[i]) 
+                NumOfBulls++; 
+            else if (Answer.ToString().Contains(Guess[i])) 
+                NumOfCows++; 
+        } 
+        Console.WriteLine("Number of bulls are " + NumOfBulls + "and the number of cows are " + NumOfCows); 
+    } while (NumOfBulls != 4); 
+    Console.WriteLine("Congratulations! You guessed it right!"); 
+} 
         }
     }
 }
